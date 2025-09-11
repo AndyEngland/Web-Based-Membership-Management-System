548e61fe97389ca2dff6e7c2371b7b70f8379663
Andy England <andyengland@hotmail.com>
2025-09-11 16:24:20 +0200

    fix(wsgi): export WSGI application callable for gunicorn

--- yzer_membership/wsgi.py
+++ yzer_membership/wsgi.py
@@ -1,1 +1,8 @@
-# ...existing code...
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Set default settings module for 'django' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yzer_membership.settings')
+
+# The WSGI application callable that gunicorn expects to find.
+application = get_wsgi_application()
